50484deffdb951024dc3e1f591610e5054379c18
Chris Zuber <shgysk8zer0@gmail.com>
2016-06-23 16:04:00 -0700

    Do actual validation on forms Exit with `$_REQUEST`, `$_FILES`, and `Form::__invoke`

--- index.php
+++ index.php
@@ -13,7 +13,10 @@ if (empty($_REQUEST['form'])) {
 	exit($dom->saveHTML());
 } elseif (file_exists("./forms/{$_REQUEST['form']}.html")) {
 	header('Content-Type: application/json');
-	exit(json_encode(Form::getFiles($_FILES)));
-	$validator = new Form($dom->getElementsByTagName('form')->item(0), Form::getUploadedFiles($_REQUEST));
-	exit(json_encode(['_files' => $_FILES, '_REQUEST' => $_REQUEST, 'validator' => $validator]));
+	$validator = new Form($dom->getElementsByTagName('form')->item(0), $_POST);
+	exit(json_encode([
+		'_files' => $_FILES,
+		'_REQUEST' => $_REQUEST,
+		'validator' => $validator()
+	]));
 }
