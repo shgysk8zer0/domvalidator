4001897869d141d8dfa52a006d09589f46b434da
Chris Zuber <shgysk8zer0@gmail.com>
2016-06-23 12:21:38 -0700

    Create and use FileNormalizer trait Uses methods previously contained in Form class

--- form.php
+++ form.php
@@ -7,6 +7,7 @@ namespace shgysk8zer0\DOMValidator;
 final class Form
 {
 	use InputValidation;
+	use FileNormalizer;
 
 	public $is_valid = true;
 	private $_submitted = [];
@@ -149,41 +150,5 @@ final class Form
 
 		$this->is_valid = empty($this->_invalid_inputs);
 		return $this->_invalid_inputs;
-	}
-
-	public static function getFiles()
-	{
-		// @see https://php.net/manual/en/features.file-upload.multiple.php#118180
-		$files = array();
-		foreach ($_FILES as $name => $values) {
-			// init for array_merge
-			if (!isset($files[$name])) {
-				$files[$name] = array();
-			}
-			if (!is_array($values['error'])) {
-				// normal syntax
-				$files[$name] = $values;
-			} else {
-				// html array feature
-				foreach ($values as $fileInfoKey => $subArray) {
-					$files[$name] = array_replace_recursive($files[$name], self::_fileWalker($subArray, $fileInfoKey));
-				}
-			}
-		}
-
-		return $files;
-	}
-
-	private static function _fileWalker(Array $arr, $fileInfokey)
-	{
-		$ret = array();
-		foreach ($arr as $k => $v) {
-			if (is_array($v)) {
-				$ret[$k] = \call_user_func(__METHOD__, $v, $fileInfokey);
-			} else {
-				$ret[$k][$fileInfokey] = $v;
-			}
-		}
-		return $ret;
 	}
 }
